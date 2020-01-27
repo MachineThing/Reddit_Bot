4c1188a45e5fdd432f5f1fb8eabd73f22814cde5
Mason Fisher <sackzombie10@gmail.com>
2020-01-27 17:38:59 -0500

    Made more advanced version checking

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 import sys
 
 if sys.version_info.major != 3:
-    print("Bad python version: \""+str(sys.version_info.major)+"\" quitting...")
+    if sys.version_info.major == 2:
+        print("Python 2 is not supported! quitting...")
+    else:
+        print("Unknown python version: \""+str(sys.version_info.major)+"\" quitting...")
     sys.exit()
-print("hi")
