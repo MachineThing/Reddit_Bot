e08f1de0960439bf159141aff43435b15290c09e
Mason Fisher <sackzombie10@gmail.com>
2020-01-27 18:19:28 -0500

    Made script read file

--- manage.py
+++ manage.py
@@ -4,12 +4,23 @@ if sys.version_info.major != 3:
     if sys.version_info.major == 2:
         print("Python 2 is not supported! quitting...")
     else:
-        print("Unknown python version: \""+str(sys.version_info.major)+"\" quitting...")
+        print("Unknown python version: \""+str(sys.version_info.major)+"\"! quitting...")
     sys.exit()
 
 if sys.argv[1] == 'init':
     botId = input("Bot ID: ")
     botSecret = input("Bot Secret: ")
-    confFile = open(".conf", 'w')
+    confFile = open('.conf', 'w')
     confFile.write(botId+'\n'+botSecret)
     confFile.close()
+elif sys.argv[1] == 'run':
+    try:
+        confFile = open('.conf', 'r')
+    except FileNotFoundError:
+        print("Configuration file not found, did you run \"python3 manage.py init\"? quitting...")
+        sys.exit()
+    lines = confFile.read().split('\n')
+    botId = lines[0]
+    botSecret = lines[1]
+    confFile.close()
+    print(botSecret)
